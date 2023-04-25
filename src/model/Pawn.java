fd95bb2c89407b67a156e1d7affd19052b9ae611
Zeky69 <zek.akburak@gmail.com>
2023-04-25 10:20:49 +0200

    TheHole

---
+++ src/model/Pawn.java
@@ -0,0 +1,33 @@
+package model;
+
+import boardifier.model.ElementTypes;
+import boardifier.model.GameElement;
+import boardifier.model.GameStageModel;
+import boardifier.model.animation.AnimationStep;
+import boardifier.view.GridGeometry;
+
+public class Pawn extends GameElement {
+
+    private int number;
+    private int color;
+    public static int PAWN_BLACK = 0;
+    public static int PAWN_RED = 1;
+
+    public Pawn(int number, int color, GameStageModel gameStageModel) {
+        super(gameStageModel);
+        // registering element types defined especially for this game
+        ElementTypes.register("pawn",50);
+        type = ElementTypes.getType("pawn");
+        this.number = number;
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+}
